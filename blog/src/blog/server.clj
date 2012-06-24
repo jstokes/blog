fde1ea3396ac5b2e0df11a24686375438121bccb
Jeff <jeffecu88@gmail.com>
2012-06-24 18:54:23 -0400

    adding mongo connectivity to startup

--- blog/src/blog/server.clj
+++ blog/src/blog/server.clj
@@ -6,6 +6,8 @@
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
+    ; TODO : factor out mongo connectivity so this works when deployed
+    (monger-connect! "mongodb://127.0.0.1/blog")
     (server/start port {:mode mode
                         :ns 'blog})))
 
